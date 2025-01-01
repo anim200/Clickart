b67350906cbc1ca95dcdc06abb2736d57657bc60
anim200 <126519717+anim200@users.noreply.github.com>
2025-01-01 23:43:33 +0600

    Update apiCalls.js

--- ecommerce2/cart/src/redux/apiCalls.js
+++ ecommerce2/cart/src/redux/apiCalls.js
@@ -5,7 +5,7 @@ export const login = async (dispatch,user)=>{
     dispatch(loginStart());
     console.log(user.username);
     try{
-        const res= await axios.post("http://localhost:5000/api/auth/login",user);
+        const res= await axios.post("https://clickart-backend.vercel.app/api/auth/login",user);
         dispatch(loginSuccess(res.data));
 
     }catch(err){
